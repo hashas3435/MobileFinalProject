6b3cb1e24e6435e2364b1b91f49bac657d32fb9b
omer hasid <omerhasid11@gmail.com>
2025-03-22 21:31:08 +0200

    set values in fields

--- app/src/main/java/com/example/firstapplication/adapter/AuctionViewHolder.kt
+++ app/src/main/java/com/example/firstapplication/adapter/AuctionViewHolder.kt
@@ -6,7 +6,11 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.firstapplication.R
 import com.example.firstapplication.databinding.AuctionListRowBinding
 import com.example.firstapplication.model.Auction
+import com.squareup.picasso.Picasso
+import java.text.SimpleDateFormat
 import java.time.LocalDate
+import java.util.Date
+import java.util.Locale
 
 interface OnItemClickListener {
     fun onItemClick(position: Int)
@@ -20,7 +24,6 @@ class AuctionViewHolder(
     private var auction: Auction? = null
 
     init {
-
         itemView.setOnClickListener {
             listener?.onItemClick(auction)
         }
@@ -31,6 +34,20 @@ class AuctionViewHolder(
         binding.auctionRowTitle.text = auction?.title
         binding.auctionRowDescription.text = auction?.description
         binding.auctionRowCurrentBid.text = "$${auction?.currentBid}"
-        binding.auctionRowEndDate.text = "${auction?.endDate}"
+        binding.auctionRowEndDate.text = getEndDateText(auction?.endDate)
+
+        if (auction?.imageUrl !== null && auction.imageUrl.isNotBlank()) {
+            Picasso.get()
+                .load(auction.imageUrl)
+                .placeholder(R.drawable.auction_logo)
+                .into(binding.auctionRowImage)
+        }
+    }
+
+    private fun getEndDateText(endDate: Long?): String {
+        if (endDate === null ){
+            return ""
+        }
+        return SimpleDateFormat("MMM dd, yyyy", Locale.ENGLISH).format(Date(endDate))
     }
 }
